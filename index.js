edd0a6031efaf00b0a49abe939058470936e5633
Alexander Esbjörnsson <alex.esbj@gmail.com>
2023-06-21 20:02:32 +0200

    fixked inports with webpack

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
-function generateString(){
+import randomString from 'random-string-esb'
+const div = document.querySelector("#random-string")
+
+function generateString() {
   const button = document.createElement("button")
-  button.innerText = ""
+  const input = document.createElement("input")
+  div.append(input, button)
+  let string = ""
+  input.setAttribute("placeholder", "Enter length of the string")
+  button.innerText = "Generate string"
+  button.addEventListener("click", () => {
+    if (!input.value) {
+      string = randomString(9)
+    }
+    else {
+      string = randomString(Number(input.value))
+    }
+    const stringHolder = document.createElement("div")
+    stringHolder.append(string)
+    div.append(stringHolder)
+  })
 }
+generateString()
